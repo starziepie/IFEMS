c57165ca9815715eab6b69a9a4deed77fdfc2f2d
SpaceGuru01 <34199284+SpaceGuru01@users.noreply.github.com>
2020-01-18 16:21:51 +1100

    Bug Fix

--- admin/header.php
+++ admin/header.php
@@ -11,7 +11,7 @@ require '../pilotarea/auth.php'
 <!DOCTYPE html>
 <html>
 <head>
-<title>InfiniteVMS</title>
+<title>IFEMS</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="../bootstrap/bootstrap.min.css">
